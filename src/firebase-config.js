edadc9cf9e0b92ef0a3b885dffb96c1fe600de8c
usamaabbasi01 <b-25991@student.usa.edu.pk>
2024-10-03 11:18:01 +0500

    forx admin

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,18 +3,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
-import { getStorage } from "firebase/storage";
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBVekboaDaux9O3J3jCqb5MTJXD2niukQg",  
-    authDomain: "forx-c7139.firebaseapp.com",  
-    projectId: "forx-c7139",  
-    storageBucket: "forx-c7139.appspot.com",  
-    messagingSenderId: "733310416513",  
-    appId: "1:733310416513:web:19ffdc9cc29d59b2637f80",  
-    measurementId: "G-WKLB4MDT4W"  
-  };
-  
+    apiKey: "AIzaSyBVekboaDaux9O3J3jCqb5MTJXD2niukQg",
+    authDomain: "forx-c7139.firebaseapp.com",
+    projectId: "forx-c7139",
+    storageBucket: "forx-c7139.appspot.com",
+    messagingSenderId: "733310416513",
+    appId: "1:733310416513:web:19ffdc9cc29d59b2637f80",
+    measurementId: "G-WKLB4MDT4W"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -22,9 +21,9 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase services
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app); // Initialize Storage
 
-// Export the db and auth
-export { db, auth };
-export const dbFirestore = getFirestore(app);   // Firestore instance
+// Export the services
+export { db, auth, storage }; // Export storage
+export const dbFirestore = getFirestore(app); // Firestore instance
 export const dbRealtime = getDatabase(app);
-const storage = getStorage(app);
